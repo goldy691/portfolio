c768b8f1934a01a1797afe94cefc6fe34b86a5bc
Goldy <mdamanullah691@gmail.com>
2024-05-12 13:30:13 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Web Designer.", "Frontend Developer.", "Full Stack Developer"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 100,
+    loop: true
+  });
